37a4f0bde9093cbd3d49f9d1d3f2af96366558e7
Maddi <maddi.fitzmaurice@gmail.com>
2025-01-08 13:01:55 +1100

    CR_DOR-5: Main Menu UI
    TASK
    - Added main menu UI skeleton
    - Set up script skeleton to handle main menu UI

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class MainMenuUI : MonoBehaviour
+{
+    private VisualElement _ui;  //Root Element
+    private Button _playButton;
+    private Button _quitButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        // Get UI Components
+        _ui = GetComponent<UIDocument>().rootVisualElement;
+        _playButton = _ui.Q<Button>("PlayButton");
+        _quitButton = _ui.Q<Button>("QuitButton");
+    }
+
+    private void OnEnable()
+    {
+        _playButton.RegisterCallback<ClickEvent>(PlayButtonHandler);
+        _quitButton.RegisterCallback<ClickEvent>(QuitButtonHandler);
+    }
+
+    private void OnDisable()
+    {
+        _playButton.UnregisterCallback<ClickEvent>(PlayButtonHandler);
+        _quitButton.UnregisterCallback<ClickEvent>(QuitButtonHandler);
+    }
+
+    #region EVENT HANDLERS
+    private void PlayButtonHandler(ClickEvent e)
+    {
+    }
+
+    private void QuitButtonHandler(ClickEvent e)
+    {
+
+    }
+    #endregion
+}
